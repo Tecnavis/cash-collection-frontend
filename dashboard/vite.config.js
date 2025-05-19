19d71d991fca844c6ef81c734a17644a07667915
thabumon <52034689+thabumon@users.noreply.github.com>
2025-05-19 22:12:21 +0530

    Update vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173, // Optional: Vite default preview port
+    host: true, // Needed to allow external access
+    allowedHosts: ['neo2.tecnavis.com'], // ✅ Add your domain here
+  },
 })
