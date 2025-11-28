d9002fa705384de62a9087bf71d275cf3885cf52
thabumon <52034689+thabumon@users.noreply.github.com>
2025-11-28 17:29:04 +0530

    Update vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,                         // allow external access
+    allowedHosts: ['neo.tecnavis.in'],  // ✅ your custom domain
+    // (optionally you can also add your Render URL here)
+    // allowedHosts: ['neo.tecnavis.in', 'your-service.onrender.com'],
+  },
 })
